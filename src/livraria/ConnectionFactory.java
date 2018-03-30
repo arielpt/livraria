752d842c626b5b0c65eb3dc5bda73dd73a4a70f6
Ariel Oliveira <ariel.ifpb@gmail.com>
2018-03-30 19:45:59 -0300

    postgres

--- src/livraria/ConnectionFactory.java
+++ src/livraria/ConnectionFactory.java
@@ -17,7 +17,7 @@ public class ConnectionFactory {
    
     public Connection getConnection(){
     Connection conn = null; try {
-    conn = DriverManager.getConnection("jdbc:postgresql:/localhost:5432/postgre", postgres, 123); } catch (SQLException e) { e.printStackTrace(); } 
+    conn = DriverManager.getConnection("jdbc:postgresql:/localhost:5432/postgre", "postgres", 123); } catch (SQLException e) { e.printStackTrace(); } 
     return conn;
      
     
