c9d337cc8bfdbe09aa35ddf3493ad39c6a82631f
Ariel Oliveira <ariel.ifpb@gmail.com>
2018-03-30 19:01:12 -0300

    factory

---
+++ src/livraria/ConnectionFactory.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package livraria;
+
+/**
+ *
+ * @author Ariel
+ */
+public class ConnectionFactory {
+    new getConnection();
+    
+    Connection conn = null; try { conn = DriverManager.getConnection(jdbc:postgresql:/localhost:5432/postgre, postgres, 123); } catch (SQLException e) { e.printStackTrace(); } return conn;
+}
