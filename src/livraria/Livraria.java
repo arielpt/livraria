5e351194c7b5fadddec3511f85d91c401a486100
Ariel Oliveira <ariel.ifpb@gmail.com>
2018-04-01 00:24:35 -0300

    feito

--- src/livraria/Livraria.java
+++ src/livraria/Livraria.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package livraria;
-
+import java.util.GregorianCalendar;
 /**
  *
  * @author ADM
@@ -17,6 +17,31 @@ public class Livraria {
     public static void main(String[] args) {
        Tecnico t = new Tecnico("Advocacia", 125446841, 2018, "Lei para tolos", "Portugues", 1);
        new TecnicoDAO().create(t);
+       
+       Autores a = new Autores("Um autor brilhante",new GregorianCalendar(1998,11,19), "Harlan Coben",  1);
+       new AutoresDAO().create(a);
+       
+       livro_autor l = new livro_autor(1,1);
+       new livro_autorDAO().create(l);
+       
+       Editoras e = new Editoras("Saraiva", 1139334000, 1);
+       new EditorasDAO().create(e);
+       
+       Edicao d = new Edicao(1, 1039, 105, 29.50, 1);
+       new EdicaoDAO().create(d);
+       
+       edicao_editora i = new edicao_editora(1,1);
+       new edicao_editoraDAO().create(i);
+       
+       livro_edicao v = new livro_edicao(1,1);
+       new livro_edicaoDAO().create(v);
+       
+       Funcionario f = new Funcionario(1, "Ariel", 201314, "Operador de Caixa");
+       new FuncionarioDAO().create(f);
+       
+       Venda n = new Venda(1, new GregorianCalendar(2018,04,01), 1, 1);
+       new VendaDAO().create(n);
+       
     }
     
 }
